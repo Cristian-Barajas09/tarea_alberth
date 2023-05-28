36f0f80a2c22945709305385b8cfdd1aaa8a45ac
alberth2003 <Alberthalejandro24@gmail.com>
2023-05-28 17:38:46 -0400

    actualizacion

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, redirect
 app = Flask(__name__)
+usuario = []
 
 
 
@@ -8,6 +9,13 @@ app = Flask(__name__)
 def home():
     return render_template("pagina productos/index.html")
 
-@app.route("/contacto")
-def about():
+@app.route("/contacto", methods =["GET", "POST"])
+def contacto():
+    if request.method == "POST":
+        usuario.append(request.form)
+        return redirect("/user")
     return render_template ("pagina productos/formulario.html")
+
+@app.route("/user")
+def user():
+    return render_template("pagina productos/user.html", usuario = usuario)
